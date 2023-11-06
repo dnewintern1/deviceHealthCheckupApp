6d683947e56a7aa5c76bf516969e9ebb5616c894
dnewintern1 <radhe3127@gmail.com>
2023-11-06 09:58:03 +0530

    mic tested

--- app/src/main/java/com/base/phonehealthcheckupapp/MicTestingActivity.java
+++ app/src/main/java/com/base/phonehealthcheckupapp/MicTestingActivity.java
@@ -2,105 +2,96 @@ package com.base.phonehealthcheckupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.AudioTrack;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 
 
-import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 
 
-
-import android.widget.Button;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import java.io.IOException;
+
+import java.io.File;
 
 public class MicTestingActivity extends AppCompatActivity {
 
-    private Button startRecordingButton;
-    private Button stopRecordingButton;
-    private AudioRecord audioRecorder;
+    MediaRecorder mediaRecorder;
 
-    int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
-    private AudioTrack audioPlayer;
+    MediaPlayer mediaPlayer;
 
-    private boolean isRecording = false;
 
-    private static final int RECORDING_DURATION_MS = 3000; // 3 seconds
-    private String audioFilePath;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mic_testing);
 
-        startRecordingButton = findViewById(R.id.startRecordingButton);
-        stopRecordingButton = findViewById(R.id.stopRecordingButton);
 
-        startRecordingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startRecording();
-            }
-        });
+        if(isMicrophonePresent()){
+            Toast.makeText(MicTestingActivity.this,"Mic Present",Toast.LENGTH_SHORT).show();
+        }
 
-        stopRecordingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopRecording();
-            }
-        });
 
-        audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/test_audio.pcm";
+
+
+
     }
 
-    @SuppressLint("MissingPermission")
-    private void startRecording() {
-        if (!isRecording) {
-            int minBufferSize = AudioRecord.getMinBufferSize(44100, MediaRecorder.AudioSource.MIC, AudioFormat.ENCODING_PCM_16BIT);
-            audioRecorder = new AudioRecord(
-                    MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize
-            );
+    public void btnRecordPressed(View v) {
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); // Choose one output format
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(getRecordingFilePath());
+            mediaRecorder.prepare();
+            mediaRecorder.start();
 
-            int bufferSize = 4096;
-            byte[] audioData = new byte[bufferSize];
-
-            audioRecorder.startRecording();
-            audioPlayer = new AudioTrack(
-                    android.media.AudioManager.STREAM_MUSIC, 44100,
-                    AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize,
-                    AudioTrack.MODE_STREAM
-            );
-            audioPlayer.play();
-
-            isRecording = true;
-            startRecordingButton.setEnabled(false);
-            stopRecordingButton.setEnabled(true);
-
-            while (isRecording) {
-                int bytesRead = audioRecorder.read(audioData, 0, bufferSize);
-                if (bytesRead > 0) {
-                    audioPlayer.write(audioData, 0, bytesRead);
-                }
-            }
+            Toast.makeText(MicTestingActivity.this, "Recording is started", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private void stopRecording() {
-        if (isRecording) {
-            audioPlayer.stop();
-            audioPlayer.release();
-
-            isRecording = false;
-            startRecordingButton.setEnabled(true);
-            stopRecordingButton.setEnabled(false);
+    public void btnStopPressed(View v) {
+        try {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            Toast.makeText(MicTestingActivity.this, "Recording is stopped", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void btnPlayPressed(View v) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getRecordingFilePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            Toast.makeText(MicTestingActivity.this, "Recording is Playing", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getRecordingFilePath() {
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File musicDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File file = new File(musicDirectory, "testRecordingFiles.3gp"); // Use the correct file extension
+        return file.getPath();
+    }
+
+    private Boolean isMicrophonePresent() {
+        return this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
     }
 }
+
+
